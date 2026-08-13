d75c5ecf4552f1362175dccd197b99ede1953e6e
Irina <vyustirina@gmail.com>
2026-08-13 23:20:27 +0300

    add scripts

--- src/server.js
+++ src/server.js
@@ -1,13 +1,28 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import pinoHttp from 'pino-http';
+import pino from 'pino-http';
 
 const app = express();
 
 const PORT = process.env.PORT ?? 3000;
 
-app.use(pinoHttp());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
 app.use(cors());
 app.use(express.json());
 
