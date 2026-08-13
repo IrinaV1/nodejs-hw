52c8710e507dd18e2f000bc0376af1e151bbce0b
Irina <vyustirina@gmail.com>
2026-08-13 22:55:55 +0300

    add pino http

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import pinoHttp from 'pino-http';
 
 const app = express();
 
 const PORT = process.env.PORT ?? 3000;
 
+app.use(pinoHttp());
 app.use(cors());
 app.use(express.json());
 
