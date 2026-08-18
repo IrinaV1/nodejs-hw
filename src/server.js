9ad467f3eb7156a2477c4a7c1f3ba6b34fd6374f
Irina <vyustirina@gmail.com>
2026-08-19 00:40:36 +0300

    add controllers, routes, db, middleware

--- src/server.js
+++ src/server.js
@@ -1,58 +1,44 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import pino from 'pino-http';
 
+import { logger } from './middleware/logger.js';
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import router from './routes/notesRoutes.js';
 const app = express();
 
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT ?? 3030;
 
-app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat:
-          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
-app.use(cors());
+app.use(logger);
 app.use(express.json());
+app.use(cors());
 
-app.get('/notes', (req, res) => {
-  res.status(200).json({
-    message: 'Retrieved all notes',
-  });
-});
+app.use(router);
 
-app.get('/notes/:noteId', (req, res) => {
-  const id_param = req.params.noteId;
-  res.status(200).json({
-    message: `Retrieved note with ID: ${id_param}`,
-  });
-});
+// app.use('/notes', (req, res) => {
+//   res.status(200).json({
+//     message: 'Retrieved all notes',
+//   });
+// });
+
+// app.use('/notes/:noteId', (req, res) => {
+//   const id_param = req.params.noteId;
+//   res.status(200).json({
+//     message: `Retrieved note with ID: ${id_param}`,
+//   });
+// });
 
 app.get('/test-error', (req, res) => {
   throw new Error('Simulated server error');
 });
 
-app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
+app.use(notFoundHandler);
 
-app.use((err, req, res, next) => {
-  console.error('Error:', err.message);
-  res.status(500).json({
-    message: err.message,
-  });
-});
+app.use(errorHandler);
+
+await connectMongoDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
